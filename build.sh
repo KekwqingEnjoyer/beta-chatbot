cd4c28021a6c585e66d775648fad42979a00e147
KekwqingEnjoyer <phandaipikachu@gmail.com>
2023-01-01 15:15:54 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 set -o errexit
 
 pip install --upgrade pip
-pip3 install PyYAML==3.13 --use-pep517
+pip install -U pip setuptools wheel
+pip install PyYAML==3.13 --use-pep517
 pip install -r requirements.txt --use-pep517
