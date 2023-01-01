a32bcbb89ca4c1e187ffe6147e2e651dda6a1f2d
KekwqingEnjoyer <phandaipikachu@gmail.com>
2023-01-01 15:30:15 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -o errexit
 
 pip install --upgrade pip
-sudo apt install libpython3.10-dev
+pip install Python.h
 pip install PyYAML==6.0 --use-pep517
 pip install -r requirements.txt --use-pep517
